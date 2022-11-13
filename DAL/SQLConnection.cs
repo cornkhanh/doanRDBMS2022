29e088938353275dbe0b1e6fc3b77fbb4a445d18
LeMinhCt <nhokbin5tuoi@gmail.com>
2022-11-13 13:35:45 +0700

    winform push len

---
+++ DAL/SQLConnection.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class SQLConnection
+    {
+        private static string m_str_Server,m_str_Database,m_str_User,m_str_Password ;
+
+        public static string ConnectionString = string.Empty;
+
+        public static void SetConnectionInfo(string str_Server, string str_Database, string str_User, string str_Password)
+        {
+            m_str_Server = str_Server;
+            m_str_Database = str_Database;
+            m_str_User = str_User;
+            m_str_Password = str_Password;
+            SetConnectionString();
+        }
+
+        private static void SetConnectionString()
+        {
+            string connectionTemplate = @"Server=myServerName\myInstanceName;Database=myDataBase;User Id=myUsername;Password=myPassword;";
+            ConnectionString = string.Format(connectionTemplate, m_str_Server, m_str_Database, m_str_User, m_str_Password);
+            //string.Format replay arg phía trên
+        }
+        public static bool TestConnection()
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(ConnectionString))
+                    {
+                        connection.Open();
+                        connection.Close();
+                        return true;
+                    }
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ TestConsole/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+
+namespace TestConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string m_str_Server = @".\LAPTOP-MJK5DKVT"
+                , m_str_Database = "quanlysanbong"
+                , m_str_User = "sa"
+                , m_str_Password = "24112002minh";
+            SQLConnection.SetConnectionInfo(m_str_Server, m_str_Database, m_str_User, m_str_Password);
+            if (SQLConnection.TestConnection())
+            {
+                Console.WriteLine("ok");
+            }
+            else
+            {
+                Console.WriteLine("fail");
+            }    
+        }
+    }
+}
+
+
